ec1f45afd4b17ea5ef9132995969ad18d83025d2
Afolayan Usman <usmanafolayan77@gmail.com>
2024-06-25 22:23:50 +0100

    kk

--- RecipeManagement System/Models/ApplicationUser.cs
+++ RecipeManagement System/Models/ApplicationUser.cs
@@ -3,13 +3,6 @@ namespace RecipeManagement_System.Models
 {
     public class ApplicationUser
     {
-        public string UserName { get; internal set; }
 
-        public string Email { get; internal set; }
-
-        public static explicit operator ApplicationUser(string v)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
